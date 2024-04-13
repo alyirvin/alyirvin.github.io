876114913fb6caf718e71f3d8612d03e593380bd
alyirvin <alysha.irvin@gmail.com>
2024-04-12 23:45:01 -0400

    experience tickets

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,6 +2,40 @@ import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 
 function Experience() {
+  const [expData, setExpData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch('/experience.json')
+    .then(response => response.json())
+    .then(expData => {
+      setExpData(expData);
+      setIsLoading(false);
+    })
+    .catch(error => console.error('Error:', error));
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  let exps;
+
+  if (expData.experiences)
+  {
+    exps = expData.experiences.map(function (exp, index) {
+      return(
+        <div className="expTicket">
+            <div className="exp-company">{exp.company}</div>
+            <div className="exp-title">{exp.title}</div>
+            <div className="exp-years">{exp.years}</div>
+            <div className="exp-desc">{exp.description}</div>
+            <div className="ticket"></div>
+        </div>
+      );
+    });
+  }
+
   return (
     <experiencepage>
       <Card>
@@ -18,6 +52,7 @@ function Experience() {
         </blockquote>
       </Card.Body>
     </Card>
+    <div className="container">{exps}</div>
   </experiencepage>
   )
 }
