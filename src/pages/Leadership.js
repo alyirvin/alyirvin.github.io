e86df002c6650c63bbb7d99b4903e3a23f28915d
alyirvin <alysha.irvin@gmail.com>
2024-04-25 16:20:30 -0400

    Condensed to three font styles

--- src/pages/Leadership.js
+++ src/pages/Leadership.js
@@ -132,8 +132,9 @@ function Leadership() {
               display: 'flex',
               flexDirection: 'column',
               justifyContent: 'space-between',
-              fontSize: '12pt',
-              padding: '10px'
+              fontSize: '11pt',
+              padding: '10px',
+              fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;'
             }}>
               {lead.description}
               <div style={{ alignSelf: 'flex-end'}}>
